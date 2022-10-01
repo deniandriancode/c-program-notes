c1107df1242165315023c6c0e7365bc28492bd83
deniandriancode <deniandriancode@gmail.com>
2022-10-01 21:33:04 +0700

    add ncurses menu

--- ncurses/menu/mymenu.c
+++ ncurses/menu/mymenu.c
@@ -1,2 +1,59 @@
 #include <menu.h>
+#include <stdlib.h>
 #include <curses.h>
+
+#define ARRAY_SIZE(a) (sizeof (a) / sizeof (a[0]))
+#define CTRL_D 4
+
+char *choices[] = {
+        "Choice 1",
+        "Choice 2",
+        "Choice 3",
+        "Choice 4",
+        "Exit"
+};
+
+int
+main (int argc, char *argv[])
+{
+        ITEM **my_items;
+        MENU *my_menu;
+        int c;
+        int n_choices, i;
+        ITEM *cur_item;
+
+        initscr ();
+        cbreak ();
+        noecho ();
+        keypad (stdscr, TRUE);
+
+        n_choices = ARRAY_SIZE (choices);
+        my_items = (ITEM **) calloc (n_choices + 1, sizeof (ITEM *));
+
+        for (i = 0; i < n_choices; i++)
+                my_items[i] = new_item (choices[i], choices[i]);
+        my_items[n_choices] = (ITEM *) NULL;
+
+        my_menu = new_menu ((ITEM **)my_items);
+        mvprintw (LINES - 2, 0, "F2 to exit");
+        post_menu (my_menu);
+        refresh ();
+
+        while ((c = getch ()) != KEY_F (2)) {
+                switch (c) {
+                        case KEY_DOWN:
+                                menu_driver (my_menu, REQ_DOWN_ITEM);
+                                break;
+                        case KEY_UP:
+                                menu_driver (my_menu, REQ_UP_ITEM);
+                                break;
+                }
+        }
+
+        free_item (my_items[0]);
+        free_item (my_items[1]);
+        free_menu (my_menu);
+
+        endwin ();
+        return 0;
+}
